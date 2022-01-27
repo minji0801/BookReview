f650c742072f22ee104a9534d907ede1d57e92d1
Minji Kim <minji_0801@naver.com>
2022-01-27 10:20:22 +0900

    Initial Commit

---
+++ BookReview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BookReview
+//
+//  Created by 김민지 on 2022/01/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
